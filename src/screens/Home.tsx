168e60c53e404d8ae14bceb2f7c237dbe595c48e
Mattia Sanfilippo <mattia.sanfilippo@netguru.com>
2023-05-19 12:28:45 +0200

    feat: add text with variable

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,6 +5,8 @@ import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 export const Home = () => {
   const {t} = useTranslation();
 
+  const todayDate = t('date', {date: new Date()});
+
   const translationsArray = t('arr', {returnObjects: true});
 
   const formattedTranslationArray = Object.values(translationsArray).map(
@@ -16,23 +18,29 @@ export const Home = () => {
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
+        <View style={styles.box}>
           <Text style={styles.title}>{t('home.title')}</Text>
           <Text style={styles.description}>{t('home.description')}</Text>
+          <Text style={styles.description}>{todayDate}</Text>
         </View>
-        {formattedTranslationArray.map((translation, index) => {
-          return (
-            <View key={index}>
-              <Text>{translation}</Text>
-            </View>
-          );
-        })}
+        <View style={styles.box}>
+          {formattedTranslationArray.map((translation, index) => {
+            return (
+              <View key={index}>
+                <Text>{translation}</Text>
+              </View>
+            );
+          })}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
